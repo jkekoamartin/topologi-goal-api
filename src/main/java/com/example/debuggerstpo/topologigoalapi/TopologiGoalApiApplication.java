b448578efce4ead289e14b1dd14c974a21dd067a
jkekoamartin <james.kekoa.martin@gmail.com>
2019-05-09 22:18:15 -0500

    Initial commit

---
+++ src/main/java/com/example/debuggerstpo/topologigoalapi/TopologiGoalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.debuggerstpo.topologigoalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopologiGoalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopologiGoalApiApplication.class, args);
+    }
+
+}
